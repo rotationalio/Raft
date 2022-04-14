90b3e530c642e8d79950815a80f0a808c8c2c966
DanielSollis <daniel@rotational.io>
2022-04-14 11:53:17 -0700

    Added cli to main

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.17
 
 require (
 	github.com/golang/protobuf v1.5.2
+	github.com/urfave/cli/v2 v2.4.0
 	google.golang.org/grpc v1.45.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 	golang.org/x/text v0.3.0 // indirect
--- server.go
+++ server.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 
 	"google.golang.org/grpc"
+
+	cli "github.com/urfave/cli/v2"
 )
 
 type server struct {
@@ -17,27 +19,53 @@ type server struct {
 }
 
 func main() {
-	fmt.Printf("foo")
+	app := cli.NewApp()
+	app.Name = "Raft"
+	app.Usage = "Rotational Lab's implementation of the Raft consensus algorithm"
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "conf-file",
+			Aliases: []string{"f"},
+			Usage:   "path to the configuration file",
+			Value:   "",
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:     "serve",
+			Usage:    "run the Traveler server",
+			Category: "server",
+			Action:   serve,
+			Flags: []cli.Flag{
+				&cli.Int64Flag{
+					Name:    "port",
+					Aliases: []string{"p"},
+					Usage:   "port to serve on",
+				},
+			},
+		},
+	}
 }
 
-func Serve(port int) (err error) {
+func serve(c *cli.Context) (err error) {
 	// Create a new gRPC Raft server
 	srv := grpc.NewServer()
 	api.RegisterRaftServer(srv, &server{})
 
 	// Create a channel to receive interupt signals and shutdown the server.
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt)
+	ch := make(chan os.Signal, 2)
+	signal.Notify(ch, os.Interrupt)
 	go func() {
-		<-c
-		fmt.Printf("\nStopping server on localhost:%d\n", port)
+		<-ch
+		fmt.Printf("\nStopping server on localhost:%d\n", c.Int("port"))
 		srv.Stop()
 		os.Exit(1)
 	}()
 
 	// Create a socket on the specified port.
 	var sock net.Listener
-	if sock, err = net.Listen("tcp", fmt.Sprintf("localhost:%d", port)); err != nil {
+	if sock, err = net.Listen("tcp", fmt.Sprintf("localhost:%d", c.Int("port"))); err != nil {
 		return err
 	}
 
@@ -72,6 +100,7 @@ func AppendEntries(ctx context.Context, req *api.AppendEntriesRequest) (rep *api
 // 2. If votedFor is null or candidateId, and candidate’s log is at
 //    least as up-to-date as receiver’s log, grant vote (sections §5.2, §5.4 of the
 //    Raft whitepaper).
+// TODO: To impliment after single-replica version
 func RequestVote(ctx context.Context, req *api.VoteRequest) (rep *api.VoteReply, err error) {
 	return &api.VoteReply{}, nil
 }
